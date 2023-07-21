c50a10ac7ab34c28c44883dba92be73b89b2d049
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2023-07-21 17:14:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-function convertToFahrenheit() {
-    const celsiusInput = document.getElementById('celsiusInput');
-    const fahrenheitInput = document.getElementById('fahrenheitInput');
-
-    if (celsiusInput.value !== '') {
-      const celsius = parseFloat(celsiusInput.value);
-      const fahrenheit = (celsius * 9 / 5) + 32;
-      fahrenheitInput.value = fahrenheit.toFixed(2);
-    } else {
-      alert('Please enter a valid Celsius temperature!');
+function convertTemperature() {
+    const inputTemp = parseFloat(document.getElementById("inputTemp").value);
+    const convertFrom = document.getElementById("convertFrom").value;
+    const convertTo = document.getElementById("convertTo").value;
+    let result;
+  
+    if (convertFrom === convertTo) {
+      result = inputTemp;
+    } else if (convertFrom === "celsius" && convertTo === "fahrenheit") {
+      result = (inputTemp * 9/5) + 32;
+    } else if (convertFrom === "fahrenheit" && convertTo === "celsius") {
+      result = (inputTemp - 32) * 5/9;
     }
+  
+    document.getElementById("result").innerText = `${inputTemp}° ${convertFrom} is ${result.toFixed(2)}° ${convertTo}`;
 }
-
-function convertToCelsius() {
-    const celsiusInput = document.getElementById('celsiusInput');
-    const fahrenheitInput = document.getElementById('fahrenheitInput');
-
-    if (fahrenheitInput.value !== '') {
-      const fahrenheit = parseFloat(fahrenheitInput.value);
-      const celsius = (fahrenheit - 32) * 5 / 9;
-      celsiusInput.value = celsius.toFixed(2);
-    } else {
-      alert('Please enter a valid Fahrenheit temperature!');
-    }
-}
+  
